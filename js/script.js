7316e1bf06d2e111d06927c6301344dc90fa75fb
JOD-J <54557008+JOD-J@users.noreply.github.com>
2021-08-05 20:32:16 +0500

    add footer add popupDelete fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 "use strict";
 
-let 
+let
+	bodyElem = document.querySelector('.body'),
 	// форма списка туду
 	todoControlElem = document.querySelector('.todo__control'),
 	// добовление туду листа
 	headerInputElem = document.querySelector('.header__input'),
-	
+
 	// кнопка добовления туду листа
 	headerButtonElem = document.querySelector('.header__button'),
 	//  контейнер туду листа
@@ -15,49 +16,52 @@ let
 	// выполненый туду лист
 	todoCompletedElem = document.querySelector('.todo__completed'),
 	// смена туду листа
-	todoСompleteElem = document.querySelector('.todo__complete');
-	console.log('headerInputElem: ', headerInputElem);
+	todoСompleteElem = document.querySelector('.todo__complete'),
+	popupContainerElem = document.querySelector('.popup__container'),
+	deleteButtonsElem = document.querySelector('.delete__buttons'),
+	deleteYesElem = document.querySelector('.delete__yes'),
+	deleteNoElem = document.querySelector('.delete__no');
 
 // отправть в localStorage метод 
 let setState = function () {
-    localStorage.setItem('todoData', JSON.stringify(todoData));
+	localStorage.setItem('todoData', JSON.stringify(todoData));
 };
 // получить из localStorage
 let getState = function () {
 	return JSON.parse(localStorage.getItem('todoData'));
 };
 
-let todoData = getState() ? getState(): [];
+let todoData = getState() ? getState() : [];
 
 // рендер страницы
 let render = function () {
 	// очищаем строки todoLisElemt
 	todoLisElemt.innerHTML = '';
 	// очищаем строки todoCompletedElem
-    todoCompletedElem.innerHTML = '';
-    todoData.forEach(function (item, index) {
+	todoCompletedElem.innerHTML = '';
+	todoData.forEach(function (item, index) {
 		// создаем элемент ли 
 		let li = document.createElement('li');
 		// присваеваем элементу ли класс todo-item
 		li.classList.add('todo__item');
 		// выводим в верстку элемент ли 
-        li.innerHTML = 
-		'<span class="text__todo">' + item.value + '</span> '+ 
-		'<div class="todo__buttons">' +
-		'<button  id="' + index + '"class="todo__remove"></button>' +
-		'<button class="todo__complete"></button>'+
-		'</div>';
+		li.innerHTML =
+			'<span class="text__todo">' + item.value + '</span> ' +
+			'<div class="todo__buttons">' +
+			'<button  id="' + index + '"class="todo__remove"></button>' +
+			'<button class="todo__complete"></button>' +
+			'</div>';
 
 		// если item.completed истина добовляеться элемент в выполненый списко туду 
-        if (item.completed) {
-            todoCompletedElem.append(li);
-        } else {
+		if (item.completed) {
+			todoCompletedElem.append(li);
+		} else {
 			// если item.completed не истиа добовляеться элемент в не выполненый списко туду 
-            todoLisElemt.append(li);
+			todoLisElemt.append(li);
 		}
 		// смена статуса туду листа
-        let todoCompleteBtn = li.querySelector('.todo__complete');
-        todoCompleteBtn.addEventListener('click', function () {
+		let todoCompleteBtn = li.querySelector('.todo__complete');
+		todoCompleteBtn.addEventListener('click', function () {
 			const animatePopup = () => {
 				let count = 0;
 				const go = () => {
@@ -75,32 +79,40 @@ let render = function () {
 			};
 			animatePopup();
 		});
+		const yesRemove = () => {
+			deleteYesElem.removeEventListener('click', yesRemove);
+			deleteNoElem.removeEventListener('click', noRemove);
+			popupContainerElem.classList.remove('popup__container-is-open');
+			const animatePopup = () => {
+				let count = 0;
+				const go = () => {
+					count += 1;
+					const animate = requestAnimationFrame(go);
+					li.classList.add('todo__item-is-open2');
+					if (count === 30) {
+						cancelAnimationFrame(animate);
+						todoData.splice(index, 1);
+						setState();
+						render();
+					}
+				};
+				requestAnimationFrame(go);
+			};
+			animatePopup();
+		};
+		const noRemove = () => {
+			deleteYesElem.removeEventListener('click', yesRemove);
+			deleteNoElem.removeEventListener('click', noRemove);
+			popupContainerElem.classList.remove('popup__container-is-open');
+		};
 		// удаление одного из элементов туду листа
 		let todoRemoveElem = li.querySelector('.todo__remove');
-		todoRemoveElem.addEventListener('click', function () {
-			let result = confirm('Вы уверенны?');
-			if (result) {
-
-				const animatePopup = () => {
-					let count = 0;
-					const go = () => {
-						count += 1;
-						const animate = requestAnimationFrame(go);
-						li.classList.add('todo__item-is-open2');
-						if (count === 30) {
-	
-							cancelAnimationFrame(animate);
-							todoData.splice(index, 1);
-							setState();
-							render();
-						}
-					};
-					requestAnimationFrame(go);
-				};
-				animatePopup();
-			}
+		todoRemoveElem.addEventListener('click', () => {
+			popupContainerElem.classList.add('popup__container-is-open');
+			deleteYesElem.addEventListener('click', yesRemove);
+			deleteNoElem.addEventListener('click', noRemove);
 		});
-    });
+	});
 };
 
 // добавление нового туду листа
@@ -108,12 +120,12 @@ todoControlElem.addEventListener('submit', function (event) {
 	//  отмена перезагрузки страницы
 	event.preventDefault();
 	// услвоие для не добовления пустых дел в туду лист
-    if ( headerInputElem.value ) {
+	if (headerInputElem.value) {
 		headerInputElem.classList.remove('header__input-is-open')
 		headerInputElem.placeholder = 'Какие планы?';
 		//  в масив todoData пуш значение headerInputElem completed-не выполенный туду лист
-        todoData.push({
-            value: headerInputElem.value,
+		todoData.push({
+			value: headerInputElem.value,
 			completed: false,
 		});
 		// очищение поле воода для туду листа после добовление их в сам туду лист 
@@ -121,7 +133,7 @@ todoControlElem.addEventListener('submit', function (event) {
 		// запуск функции 
 		setState();
 		render();
-    } else {
+	} else {
 		headerInputElem.classList.add('header__input-is-open');
 		headerInputElem.placeholder = 'Поле не может быть пустым!!!';
 	}
@@ -129,14 +141,3 @@ todoControlElem.addEventListener('submit', function (event) {
 
 // запуск функции 
 render();
-
-
-
-
-
-
-
-
-
-
-
